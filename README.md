61f45b1bf2f88fd9c7de90ac43932dbe01cd43b7
Tim Siwula <tcsiwula@gmail.com>
2018-11-25 22:38:21 -0600

    created simple template index.html

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 # start app server
 ```
+virtualenv ~/.venvs/lpthw
+. ~/.venvs/lpthw/bin/activate
 python3 setup.py install
 python3 app.py
 open http://127.0.0.1:5000
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world, from flask app.py'
+    greeting = 'Hello world, from flask app.py'
+    return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
     app.run()
